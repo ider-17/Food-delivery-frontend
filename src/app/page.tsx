5ebbd0c0bd5773e150fd6e58d49373029c90a1d5
ider <ider@iders-MacBook-Pro.local>
2025-03-26 22:08:05 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
       <Header />
 
-      <img src="./home-bg.svg" />
+      <img className="w-full" src="./home-bg.svg" />
 
       <div className="py-8 px-12">
         <p className="text-white font-semibold mb-4 ml-10">Categories</p>
@@ -38,37 +38,41 @@ export default function Home() {
 
       <div className="py-10 px-22">
         <p className="text-white font-semibold mb-4">Appetizers</p>
-        <div className="grid grid-cols-3 grid-rows-2 gap-9">
+        <div className="grid grid-cols-3 grid-rows-2 gap-9 w-full">
 
           <Dialog>
             <DialogTrigger>
               <div className="bg-white p-4 rounded-[20px]">
                 <div className="relative">
-                  <img src="./finger-food.svg" />
-                  <Button className="bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white" variant="secondary">+</Button>
+                  <img className="rounded-sm w-full" src="./finger-food.svg" />
+                  <div className="flex justify-center items-center bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white">+</div>
                 </div>
                 <div className="flex justify-between mt-5">
                   <p className="text-[#EF4444] font-semibold">Finger food</p>
                   <p>$12.99</p>
                 </div>
-                <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
+                <p className="mt-2 text-left">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
               </div>
             </DialogTrigger>
             <DialogContent>
               <DialogHeader>
                 {/* <DialogTitle>Are you absolutely sure?</DialogTitle> */}
                 <DialogDescription>
-                  <div className="flex gap-6">
-                    <img className="w-[300px]" src="./finger-food.svg" />
+                  <div className="flex">
+                    <img className="w-1/2" src="./finger-food.svg" />
                     <div className="p-4 w-[250px]">
                       <h5 className="text-[#EF4444] font-semibold mb-4">Sunshine Stackers</h5>
-                      <p>Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-                      <div className="flex justify-between">
+                      <p className="mb-20">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
+                      <div className="flex justify-between items-center">
                         <div>
                           <p>Total price</p>
-                          <p>$12.99</p>
+                          <p className="text-black font-semibold">$12.99</p>
                         </div>
-
+                        <div className="flex gap-2 items-center mt-10">
+                          <div className="w-10 h-10 rounded-full bg-white flex justify-center items-center border ">-</div>
+                          <p>1</p>
+                          <div className="w-10 h-10 rounded-full bg-white flex justify-center items-center border ">+</div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -76,69 +80,6 @@ export default function Home() {
               </DialogHeader>
             </DialogContent>
           </Dialog>
-
-
-
-
-          <div className="bg-white p-4 rounded-[20px]">
-            <div className="relative">
-              <img src="./finger-food.svg" />
-              <Button className="bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white" variant="secondary">+</Button>
-            </div>
-            <div className="flex justify-between mt-5">
-              <p className="text-[#EF4444] font-semibold">Finger food</p>
-              <p>$12.99</p>
-            </div>
-            <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-          </div>
-
-          <div className="bg-white p-4 rounded-[20px]">
-            <div className="relative">
-              <img src="./finger-food.svg" />
-              <Button className="bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white" variant="secondary">+</Button>
-            </div>
-            <div className="flex justify-between mt-5">
-              <p className="text-[#EF4444] font-semibold">Finger food</p>
-              <p>$12.99</p>
-            </div>
-            <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-          </div>
-
-          <div className="bg-white p-4 rounded-[20px]">
-            <div className="relative">
-              <img src="./finger-food.svg" />
-              <Button className="bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white" variant="secondary">+</Button>
-            </div>
-            <div className="flex justify-between mt-5">
-              <p className="text-[#EF4444] font-semibold">Finger food</p>
-              <p>$12.99</p>
-            </div>
-            <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-          </div>
-
-          <div className="bg-white p-4 rounded-[20px]">
-            <div className="relative">
-              <img src="./finger-food.svg" />
-              <Button className="bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white" variant="secondary">+</Button>
-            </div>
-            <div className="flex justify-between mt-5">
-              <p className="text-[#EF4444] font-semibold">Finger food</p>
-              <p>$12.99</p>
-            </div>
-            <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-          </div>
-
-          <div className="bg-white p-4 rounded-[20px]">
-            <div className="relative">
-              <img src="./finger-food.svg" />
-              <Button className="bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white" variant="secondary">+</Button>
-            </div>
-            <div className="flex justify-between mt-5">
-              <p className="text-[#EF4444] font-semibold">Finger food</p>
-              <p>$12.99</p>
-            </div>
-            <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-          </div>
 
         </div>
       </div>
