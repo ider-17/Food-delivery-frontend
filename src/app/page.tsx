64aebe240dbd8767b19aea5dc54a299b267fb7ba
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-03-27 12:47:48 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Card } from "@/components/ui/card"
+
 
 export default function Home() {
   return (
@@ -40,46 +42,24 @@ export default function Home() {
         <p className="text-white font-semibold mb-4">Appetizers</p>
         <div className="grid grid-cols-3 grid-rows-2 gap-9 w-full">
 
-          <Dialog>
-            <DialogTrigger>
-              <div className="bg-white p-4 rounded-[20px]">
-                <div className="relative">
-                  <img className="rounded-sm w-full" src="./finger-food.svg" />
-                  <div className="flex justify-center items-center bg-white text-[#EF4444] rounded-full absolute bottom-[5%] right-[5%] w-11 h-11 hover:bg-[#EF4444] hover:text-white">+</div>
-                </div>
-                <div className="flex justify-between mt-5">
-                  <p className="text-[#EF4444] font-semibold">Finger food</p>
-                  <p>$12.99</p>
-                </div>
-                <p className="mt-2 text-left">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-              </div>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                {/* <DialogTitle>Are you absolutely sure?</DialogTitle> */}
-                <DialogDescription>
-                  <div className="flex">
-                    <img className="w-1/2" src="./finger-food.svg" />
-                    <div className="p-4 w-[250px]">
-                      <h5 className="text-[#EF4444] font-semibold mb-4">Sunshine Stackers</h5>
-                      <p className="mb-20">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
-                      <div className="flex justify-between items-center">
-                        <div>
-                          <p>Total price</p>
-                          <p className="text-black font-semibold">$12.99</p>
-                        </div>
-                        <div className="flex gap-2 items-center mt-10">
-                          <div className="w-10 h-10 rounded-full bg-white flex justify-center items-center border ">-</div>
-                          <p>1</p>
-                          <div className="w-10 h-10 rounded-full bg-white flex justify-center items-center border ">+</div>
-                        </div>
-                      </div>
-                    </div>
+
+          {/* <Dialog>
+            <DialogTrigger asChild>
+              <Card className="p-4">
+                <div>
+                  <img className="rounded-sm" src="./finger-food.svg" />
+                  <div className="flex justify-between mt-5">
+                    <h6 className="text-[#EF4444] font-semibold">Sunshine Stachers</h6>
+                    <span className="font-semibold">$12.99</span>
                   </div>
-                </DialogDescription>
-              </DialogHeader>
+                  <p className="mt-2">Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.</p>
+                </div>
+              </Card>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[500px]">
             </DialogContent>
-          </Dialog>
+          </Dialog> */}
+
 
         </div>
       </div>
