db4f5d078ef35f8feb90ccd8047ac236845affc1
ider-17 <24LP6042@G303-CC0142.local>
2025-04-04 12:44:16 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,3 +89,77 @@ export default function Home() {
     </div >
   );
 }
+
+
+// /* <ChevronLeft color="#FFFFFF" className="mt-1" /> */}
+// <div className="flex gap-2 items-center overflow-x-auto whitespace-nowrap pb-4 px-10">
+
+// <button className="bg-[#EF4444] text-white py-1 px-3 rounded-2xl">Appetizers</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Salads</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Pizzas</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Lunch favorites</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Main dishes</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Fish * Sea foods</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Side dish</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Brunch</button>
+// <button className="bg-white py-1 px-3 rounded-2xl">Desserts</button>
+
+// </div>
+// {/* <ChevronRight color="#FFFFFF" className="mt-1" /> */}
+// </div>
+// </div>
+
+// <div className="py-10 px-4 min-sm:px-22">
+// <p className="text-white font-semibold mb-4">Appetizers</p>
+// <div className="flex flex-col gap-8 sm:grid-cols-2 min-sm:grid min-lg:grid-cols-3 min-md:grid-rows-2 min-sm:gap-9 w-full">
+
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+
+// </div>
+// </div>
+
+// <div className="py-10 px-4 min-sm:px-22">
+// <p className="text-white font-semibold mb-4">Salads</p>
+// <div className="flex flex-col gap-8 sm:grid-cols-2 min-sm:grid min-lg:grid-cols-3 max-sm:grid-rows-2 min-sm:gap-9 w-full">
+
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+
+// </div>
+// </div>
+
+// <div className="py-10 px-4 min-sm:px-22">
+// <p className="text-white font-semibold mb-4">Lunch favorites</p>
+// <div className="flex flex-col gap-8 sm:grid-cols-2 min-sm:grid min-lg:grid-cols-3 min-md:grid-rows-2 min-sm:gap-9 w-full">
+
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+
+// </div>
+// </div>
+
+// <div className="py-10 px-4 min-sm:px-22">
+// <p className="text-white font-semibold mb-4">Salads</p>
+// <div className="flex flex-col gap-8 sm:grid-cols-2 min-sm:grid min-lg:grid-cols-3 max-sm:grid-rows-2 min-sm:gap-9 w-full">
+
+// <FoodDialog />
+// <FoodDialog />
+// <FoodDialog />
+
+// </div>
+// </div>
+
+// <Footer />
+
+// </div >
+// );
+// }
