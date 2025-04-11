7915286cb9695e3547cfece63c570d0bc3fdccf2
ider-17 <ider7037@gmail.com>
2025-04-11 12:44:55 +0800

    up

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -18,10 +18,10 @@ export default function LogInPage() {
             <p className="mb-3">Sign up to explore your favorite dishes.</p>
             <Input className="mb-3" />
             <Button className="bg-black text-white w-full" variant="outline">Let's Go</Button>
-            <div className="flex gap-1 justify-self-center">
+            {/* <div className="flex gap-1 justify-self-center">
               <Button className="text-gray-500 font-normal p-0 m-0" variant="link">Already have an account?</Button>
               <Button className="text-blue-500 font-normal p-0 m-0" variant="link">Log in</Button>
-            </div>
+            </div> */}
           </div>
 
         </div>
