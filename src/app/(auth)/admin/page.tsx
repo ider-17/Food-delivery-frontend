17bb29b967136787b071693f87971dec9f494f93
ider-17 <ider7037@gmail.com>
2025-04-17 13:01:21 +0800

    up

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -2,22 +2,40 @@
 
 import { Button } from "@/components/ui/button";
 import { BASE_URL } from "@/constants";
-import { LayoutDashboard, Settings, Truck } from "lucide-react";
+import { CircleUserRound, LayoutDashboard, Settings, Truck } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type CategoryType = {
+    name: string;
+    _id: string;
+    foods: [];
+}
 
 export default function AdminPage() {
-    async function categoryFetch() {
-        const response = await fetch(`${BASE_URL}/categories`);
+
+    const [categories, setCategories] = useState([])
+
+    const categoryFetch = async () => {
+        const response = await fetch(`${BASE_URL}/categories/with-foods`);
+
         const { categories } = await response.json();
 
-        console.log(categories, "res")
+        console.log(categories);
+
+        setCategories(categories);
+
     };
+
+    useEffect(() => {
+        categoryFetch();
+    }, []);
 
     return (
         <div className="w-full h-screen flex bg-[#F4F4F5]">
-            <div className="w-[205px] bg-white flex flex-col items-center">
+            <div className="bg-white flex flex-col items-center px-5 py-9">
                 <Link href="/">
-                    <div className="w-fit cursor-pointer mt-9">
+                    <div className="w-fit cursor-pointer">
                         <img className="w-[165px] h-[44px]" src="./admin-logo.svg" />
                     </div>
                 </Link>
@@ -32,8 +50,30 @@ export default function AdminPage() {
                     <Settings /> Settings
                 </div>
             </div>
-            <div className="p-6">
-                <div className="bg-white">
+
+            <div className="p-6 w-full flex flex-col gap-6">
+                <div className="bg-white rounded-xl w-full p-6">
+                    <div className="flex justify-end">
+                        <CircleUserRound className="rounded full" size={40} />
+                    </div>
+                    <h1 className="font-semibold text-2xl">Dishes category</h1>
+                    <div className="flex gap-3 w-full flex-wrap mt-4">
+                        {categories.map((c: CategoryType) => {
+                            return (
+                                <div key={c._id} className="border rounded-full py-2 px-4 flex gap-2">
+                                    <p>{c.name}</p>
+                                    <p className="text-white bg-black px-3 rounded-full">{c.foods.length}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className="bg-white rounded-xl p-5 flex flex-col gap-4">
+                    <h1 className="font-semibold text-2xl">Appetizers</h1>
+                    <div>
+
+                    </div>
                 </div>
             </div>
         </div>
